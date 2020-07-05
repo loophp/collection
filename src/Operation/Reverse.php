a9b6c9ab5343968ebaa99ef1802bc0de6c590ca2
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-07-05 18:59:54 +0200

    Update Reverse operation.

--- src/Operation/Reverse.php
+++ src/Operation/Reverse.php
@@ -8,16 +8,20 @@ use Closure;
 use Generator;
 use loophp\collection\Contract\Operation;
 use loophp\collection\Transformation\All;
+use loophp\collection\Transformation\Run;
+use loophp\collection\Transformation\Transform;
 
 final class Reverse extends AbstractOperation implements Operation
 {
     public function __invoke(): Closure
     {
         return static function (iterable $collection): Generator {
-            $all = (new All())($collection);
+            $all = (new Transform(new All()))((new Run(new Wrap()))($collection));
 
-            for (end($all); null !== ($key = key($all)); prev($all)) {
-                yield $key => current($all);
+            for (end($all); null !== key($all); prev($all)) {
+                $item = current($all);
+
+                yield key($item) => current($item);
             }
         };
     }
