20e3573e81e4267ae0e1dac95cbe9c52b012fdb0
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-07-05 17:59:13 +0200

    Update Chunk operation.

--- src/Operation/Chunk.php
+++ src/Operation/Chunk.php
@@ -6,9 +6,9 @@ namespace loophp\collection\Operation;
 
 use Closure;
 use Generator;
-use loophp\collection\Collection;
 use loophp\collection\Contract\Operation;
 use loophp\collection\Iterator\IterableIterator;
+use loophp\collection\Transformation\Run;
 
 use function count;
 
@@ -26,24 +26,24 @@ final class Chunk extends AbstractOperation implements Operation
              * @param array<int, int> $sizes
              */
             static function (iterable $collection, array $sizes): Generator {
-                $sizes = new IterableIterator(
-                    (new Collection($sizes))->loop()
+                $sizesIterator = new IterableIterator(
+                    (new Run(new Loop()))($sizes)
                 );
 
                 $values = [];
 
                 foreach ($collection as $value) {
-                    if (0 >= $sizes->current()) {
+                    if (0 >= $sizesIterator->current()) {
                         return yield from [];
                     }
 
-                    if (count($values) !== $sizes->current()) {
+                    if (count($values) !== $sizesIterator->current()) {
                         $values[] = $value;
 
                         continue;
                     }
 
-                    $sizes->next();
+                    $sizesIterator->next();
 
                     yield $values;
 
