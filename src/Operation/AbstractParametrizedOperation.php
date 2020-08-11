7adab652d20c0d71bf9219ac6ebe1d59c09b452e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-08-11 18:00:00 +0200

    Remove Operation::get().

--- src/Operation/AbstractParametrizedOperation.php
+++ src/Operation/AbstractParametrizedOperation.php
@@ -6,26 +6,12 @@ namespace loophp\collection\Operation;
 
 use Generator;
 
-use function array_key_exists;
-
 abstract class AbstractParametrizedOperation
 {
     /**
      * @var array<string, mixed>
      */
     protected $storage = [];
-
-    /**
-     * @param mixed|null $default
-     *
-     * @return mixed|null
-     */
-    public function get(string $key, $default = null)
-    {
-        return array_key_exists($key, $this->storage) ?
-            $this->storage[$key] :
-            $default;
-    }
 
     /**
      * @psalm-return Generator<int, mixed>
