1702597065648df7006c87a06becf5bf5e4827d2
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-08-25 18:36:28 +0200

    Update Map operation.

--- src/Operation/Map.php
+++ src/Operation/Map.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace loophp\collection\Operation;
 
-use ArrayIterator;
 use Closure;
 use Generator;
 use Iterator;
 use loophp\collection\Contract\Operation;
-use loophp\collection\Transformation\FoldLeft;
-use loophp\collection\Transformation\Transform;
 
 /**
  * @psalm-template TKey
@@ -21,25 +18,25 @@ final class Map extends AbstractOperation implements Operation
 {
     public function __construct(callable ...$callbacks)
     {
-        $this->storage['callbacks'] = new ArrayIterator($callbacks);
+        $this->storage['callbacks'] = $callbacks;
     }
 
     public function __invoke(): Closure
     {
         return
             /**
-             * @psalm-param \Iterator<TKey, T> $iterator
+             * @psalm-param Iterator<TKey, T> $iterator
              * @psalm-param list<callable(T, TKey):(T)> $callbacks
              *
-             * @psalm-return \Generator<TKey, T>
+             * @psalm-return Generator<TKey, T>
              */
-            static function (Iterator $iterator, ArrayIterator $callbacks): Generator {
+            static function (Iterator $iterator, array $callbacks): Generator {
                 foreach ($iterator as $key => $value) {
                     $callback = static function ($carry, callable $callback) use ($value, $key) {
                         return $callback($value, $key);
                     };
 
-                    yield $key => (new Transform(new FoldLeft($callback, $value)))($callbacks);
+                    yield $key => array_reduce($callbacks, $callback, $value);
                 }
             };
     }
