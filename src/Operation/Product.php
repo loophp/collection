1318e41c8b88e5e248c45f0d20ed48f592f842e1
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-09-09 23:11:23 +0200

    refactor: Update Product operation.

--- src/Operation/Product.php
+++ src/Operation/Product.php
@@ -20,41 +20,54 @@ final class Product extends AbstractOperation
 {
     public function __invoke(): Closure
     {
-        $cartesian = function (array $input): Generator {
-            return $this->cartesian($input);
-        };
+        return
+            /**
+             * @psalm-param iterable<TKey, T> ...$iterables
+             *
+             * @psalm-return Closure(Iterator<TKey, T>): Generator<int, array<int, T>>
+             */
+            static function (iterable ...$iterables): Closure {
+                return
+                    /**
+                     * @psalm-param Iterator<TKey, T> $iterator
+                     *
+                     * @psalm-return Generator<int, array<int, T>>
+                     */
+                    static function (Iterator $iterator) use ($iterables): Generator {
+                        /** @psalm-var Closure(iterable<TKey, T>...): Generator<int, array<int, T>> $cartesian */
+                        $cartesian =
+                            /**
+                             * @param array<int, iterable> ...$iterables
+                             *
+                             * @psalm-param iterable<TKey, T> ...$iterables
+                             *
+                             * @psalm-return Generator<int, array<int, T>>
+                             */
+                            static function (iterable ...$iterables) use (&$cartesian): Generator
+                            {
+                                $iterable = array_pop($iterables);
 
-        return static function (iterable ...$iterables) use ($cartesian): Closure {
-            return static function (Iterator $iterator) use ($cartesian, $iterables): Generator {
-                $its = [$iterator];
+                                if (null === $iterable) {
+                                    return yield [];
+                                }
 
-                foreach ($iterables as $iterable) {
-                    $its[] = new IterableIterator($iterable);
-                }
+                                // @todo Find better algo, without recursion.
+                                /** @psalm-var array<int, T> $item */
+                                foreach ($cartesian(...$iterables) as $item) {
+                                    foreach ($iterable as $value) {
+                                        yield $item + [count($item) => $value];
+                                    }
+                                }
+                            };
 
-                return yield from $cartesian($its);
+                        $iterators = [$iterator];
+
+                        foreach ($iterables as $iterable) {
+                            $iterators[] = $iterable;
+                        }
+
+                        return yield from $cartesian(...$iterators);
+                    };
             };
-        };
-    }
-
-    /**
-     * @param array<int, iterable> $iterators
-     * @psalm-param array<int, Iterator<TKey, T>> $iterators
-     *
-     * @psalm-return Generator<int, array<int, T>>
-     */
-    private function cartesian(array $iterators): Generator
-    {
-        $iterator = array_pop($iterators);
-
-        if (null === $iterator) {
-            return yield [];
-        }
-
-        foreach ($this->cartesian($iterators) as $item) {
-            foreach ($iterator as $value) {
-                yield $item + [count($item) => $value];
-            }
-        }
     }
 }
