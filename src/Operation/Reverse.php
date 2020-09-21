6ce7b13e674f3c4105a360850a8c6bb541760579
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-09-21 06:16:16 +0200

    refactor: Update Reverse operation.

--- src/Operation/Reverse.php
+++ src/Operation/Reverse.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace loophp\collection\Operation;
 
+use ArrayIterator;
 use Closure;
 use Generator;
 use Iterator;
@@ -12,6 +13,8 @@ use Iterator;
  * @psalm-template TKey
  * @psalm-template TKey of array-key
  * @psalm-template T
+ *
+ * phpcs:disable Generic.WhiteSpace.ScopeIndent.IncorrectExact
  */
 final class Reverse extends AbstractOperation
 {
@@ -27,15 +30,37 @@ final class Reverse extends AbstractOperation
              * @psalm-return Generator<TKey, T, mixed, void>
              */
             static function (Iterator $iterator): Generator {
-                /** @psalm-var Generator<int, array{0: TKey, 1: T}> $pack */
-                $pack = Pack::of()($iterator);
-                $all = iterator_to_array($pack);
+                /** @psalm-var Generator<int, array{0: TKey, 1: T}> $iterator */
+                $iterator = Pack::of()($iterator);
+                /** @psalm-var Generator<int, array{0: array{0: TKey, 1: T}}> $iterator */
+                $iterator = Wrap::of()($iterator);
 
-                for (end($all); null !== key($all); prev($all)) {
-                    $item = current($all);
+                /**
+                 * @param array $carry
+                 * @psalm-param array<int, array{0: TKey, 1: T}> $carry
+                 *
+                 * @param array $value
+                 * @psalm-param array{0: TKey, 1: T} $value
+                 *
+                 * @psalm-return array<int, array{0: TKey, 1: T}>
+                 */
+                $callback = static function (array $carry, array $value): array {
+                    array_unshift($carry, ...$value);
 
-                    yield $item[0] => $item[1];
+                    return $carry;
+                };
+
+                if (!$iterator->valid()) {
+                    return yield from [];
                 }
+
+                /** @psalm-var Iterator<int, array{0: TKey, 1: T}> $foldLeft1 */
+                $foldLeft1 = FoldLeft1::of()($callback)($iterator);
+
+                /** @psalm-var Iterator<TKey, T> $unpack */
+                $unpack = Unpack::of()(new ArrayIterator($foldLeft1->current()));
+
+                return yield from $unpack;
             };
     }
 }
