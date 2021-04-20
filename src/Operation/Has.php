14253435b5577e47ee51753ab1641a7bef8ef745
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-04-20 14:31:13 +0200

    refactor: Improve Has operation.

--- src/Operation/Has.php
+++ src/Operation/Has.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace loophp\collection\Operation;
 
-use ArrayIterator;
 use Closure;
 use Generator;
 use Iterator;
@@ -30,43 +29,26 @@ final class Has extends AbstractOperation
              * @psalm-return Closure(Iterator<TKey, T>): Generator<int|TKey, bool>
              */
             static function (callable ...$callbacks): Closure {
-                return
-                    /**
-                     * @psalm-param Iterator<TKey, T> $iterator
-                     *
-                     * @psalm-return Generator<int|TKey, bool>
-                     */
-                    static function (Iterator $iterator) use ($callbacks): Generator {
-                        /** @psalm-var list<Closure(T, TKey, Iterator<TKey, T>): bool> $callbacks */
-                        $callbacks = array_map(
+                /** @psalm-var Closure(Iterator<TKey, T>): Generator<int|TKey, bool> $pipe */
+                $pipe = MatchOne::of()(static fn (): bool => true)(
+                    ...array_map(
+                        static fn (callable $callback): callable =>
                             /**
-                             * @psalm-param callable(T, TKey, Iterator<TKey, T>): bool $callback
+                             * @param mixed $value
+                             * @psalm-param T $value
+                             *
+                             * @param mixed $key
+                             * @psalm-param TKey $key
+                             *
+                             * @psalm-param Iterator<TKey, T> $iterator
                              */
-                            static fn (callable $callback): Closure =>
-                                /**
-                                 * @param mixed $value
-                                 * @psalm-param T $value
-                                 *
-                                 * @param mixed $key
-                                 * @psalm-param TKey $key
-                                 *
-                                 * @psalm-param Iterator<TKey, T> $iterator
-                                 */
-                                static fn ($value, $key, Iterator $iterator): bool => $callback($value, $key, $iterator) === $value,
-                            $callbacks
-                        );
+                            static fn ($value, $key, Iterator $iterator): bool => $callback($value, $key, $iterator) === $value,
+                        $callbacks
+                    )
+                );
 
-                        foreach ($iterator as $key => $current) {
-                            /** @psalm-var Iterator<int, bool> $result */
-                            $result = MatchOne::of()(static fn (): bool => true)(...$callbacks)(Pair::of()(new ArrayIterator([$key, $current])));
-
-                            if (true === $result->current()) {
-                                return yield $key => true;
-                            }
-                        }
-
-                        return yield false;
-                    };
+                // Point free style.
+                return $pipe;
             };
     }
 }
