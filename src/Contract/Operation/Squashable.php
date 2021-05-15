93f86a7fbcb8c25e862cdc2719dd8b298565ec3b
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-05-15 22:57:28 +0200

    Add Interfaces.

---
+++ src/Contract/Operation/Squashable.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace loophp\collection\Contract\Operation;
+
+use loophp\collection\Contract\Collection;
+
+/**
+ * @psalm-template TKey
+ * @psalm-template TKey of array-key
+ * @psalm-template T
+ */
+interface Squashable
+{
+    /**
+     * @psalm-return \loophp\collection\Collection<TKey, T>
+     */
+    public function squash(): Collection;
+}
