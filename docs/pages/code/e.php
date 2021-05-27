4188454e38fb76bb53c2a1f0e247cb315fc31772
AlexandruGG <alex.gidei@goodlord.co>
2021-05-27 18:43:14 +0100

    Fix long line

--- docs/pages/code/e.php
+++ docs/pages/code/e.php
@@ -15,9 +15,11 @@ $multiplication = static fn (float $value1, float $value2): float => $value1 * $
 
 $addition = static fn (float $value1, float $value2): float => $value1 + $value2;
 
-$fact = static fn (float $number): float => (float) Collection::range(1, $number + 1)->foldLeft($multiplication, 1)->current();
+$fact = static fn (float $number): float => (float) Collection::range(1, $number + 1)
+    ->foldLeft($multiplication, 1)
+    ->current();
 
-$number_e_approximation = Collection::unfold(static fn(int $i = 0): int => $i + 1)
+$number_e_approximation = Collection::unfold(static fn (int $i = 0): int => $i + 1)
     ->map(static fn (float $value): float => $value / $fact($value))
     ->until(static fn (float $value): bool => 10 ** -12 > $value)
     ->foldLeft($addition, 0)
