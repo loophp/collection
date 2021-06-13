25d40299d729382403a97f79d6eb44afbcdc38a7
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-06-13 08:20:12 +0200

    feat: Add When operation. (#105)
    * feat: Add When operation.
    
    * Add tests.
    
    * docs: Add documentation.
    
    * feat: Add more typing information.

---
+++ src/Contract/Operation/Whenable.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace loophp\collection\Contract\Operation;
+
+use Iterator;
+use loophp\collection\Contract\Collection;
+
+/**
+ * @template TKey
+ * @template T
+ */
+interface Whenable
+{
+    /**
+     * @param callable(Iterator<TKey, T>): bool $predicate
+     * @param callable(Iterator<TKey, T>): iterable<TKey, T> $whenTrue
+     * @param callable(Iterator<TKey, T>): iterable<TKey, T> $whenFalse
+     *
+     * @return \loophp\collection\Collection<TKey, T>
+     */
+    public function when(callable $predicate, callable $whenTrue, ?callable $whenFalse = null): Collection;
+}
