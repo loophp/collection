3a426cd00baeadf09d22d2b5a4e2d7b02d295b67
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-07-05 11:04:10 +0200

    feat: Add Matching operation and implements Selectable from Doctrine. (#115)
    * feat: Add Matching operation and implements Selectable from Doctrine.
    
    * Add tests.
    
    * Update PSalm and PHPStan baselines.
    
    * Update docs/pages/api.rst
    
    Co-authored-by: Alex Gidei <34811569+AlexandruGG@users.noreply.github.com>
    
    * Update src/Contract/Operation/Matchingable.php
    
    Co-authored-by: Alex Gidei <34811569+AlexandruGG@users.noreply.github.com>
    
    * Update code based on review.
    
    * Add static analysis tests.
    
    * Do not extend Selectable.
    
    * Update documentation.
    
    * Update Psalm baseline
    
    Co-authored-by: Alex Gidei <34811569+AlexandruGG@users.noreply.github.com>
    Co-authored-by: AlexandruGG <alex.gidei@goodlord.co>

---
+++ src/Operation/Matching.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace loophp\collection\Operation;
+
+use Closure;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Collections\Expr\ClosureExpressionVisitor;
+use Generator;
+use Iterator;
+use loophp\collection\Contract\Operation\Sortable;
+
+/**
+ * @template TKey
+ * @template T
+ *
+ * phpcs:disable Generic.Files.LineLength.TooLong
+ */
+final class Matching extends AbstractOperation
+{
+    /**
+     * @return Closure(Criteria): Closure(Iterator<TKey, T>): Generator<TKey, T>
+     */
+    public function __invoke(): Closure
+    {
+        return
+            /**
+             * @return Closure(Iterator<TKey, T>): Generator<TKey, T>
+             */
+            static function (Criteria $criteria): Closure {
+                $expr = $criteria->getWhereExpression();
+                $pipes = [];
+
+                if (null !== $expr) {
+                    $pipes[] = Filter::of()((new ClosureExpressionVisitor())->dispatch($expr));
+                }
+
+                $orderings = $criteria->getOrderings();
+
+                if ([] !== $orderings) {
+                    $next = null;
+
+                    foreach (array_reverse($orderings) as $field => $ordering) {
+                        $next = ClosureExpressionVisitor::sortByField($field, Criteria::DESC === $ordering ? -1 : 1, $next);
+                    }
+
+                    $pipes[] = Sort::of()(Sortable::BY_VALUES)($next);
+                }
+
+                $offset = $criteria->getFirstResult();
+                $length = $criteria->getMaxResults();
+
+                if (null !== $offset || null !== $length) {
+                    $pipes[] = Limit::of()($length)((int) $offset);
+                }
+
+                /** @var Closure(Iterator<TKey, T>): Generator<TKey, T> $pipe */
+                $pipe = Pipe::of()(...$pipes);
+
+                // Point free style.
+                return $pipe;
+            };
+    }
+}
