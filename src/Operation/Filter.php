e92d12cd2c0df63df1143e09c89b9e8743b48aae
AlexandruGG <alex.gidei@goodlord.co>
2021-07-06 22:36:33 +0100

    Fix weird spaces

--- src/Operation/Filter.php
+++ src/Operation/Filter.php
@@ -26,7 +26,7 @@ final class Filter extends AbstractOperation
     /**
      * @pure
      *
-     * @return Closure(callable(T , TKey, Iterator<TKey, T>): bool ...): Closure (Iterator<TKey, T>): Generator<TKey, T>
+     * @return Closure(callable(T, TKey, Iterator<TKey, T>): bool ...): Closure(Iterator<TKey, T>): Generator<TKey, T>
      */
     public function __invoke(): Closure
     {
