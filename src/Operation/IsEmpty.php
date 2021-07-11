098d627d689a3196a4af7d385c2940e49bd6a3f2
AlexandruGG <alex.gidei@goodlord.co>
2021-07-11 21:17:27 +0100

    Add IsEmpty Operation

---
+++ src/Operation/IsEmpty.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace loophp\collection\Operation;
+
+use Closure;
+use Iterator;
+
+/**
+ * @immutable
+ *
+ * @template TKey
+ * @template T
+ */
+final class IsEmpty extends AbstractOperation
+{
+    /**
+     * @pure
+     *
+     * @return Closure(Iterator<TKey, T>): bool
+     */
+    public function __invoke(): Closure
+    {
+        return static fn (Iterator $iterator): bool => !$iterator->valid();
+    }
+}
