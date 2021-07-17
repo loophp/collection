a48b9e5bfe89bb7b0dfcda7c902e62f5717cdfc0
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-07-17 20:40:19 +0200

    SA: Add Tail static analysis tests.

---
+++ tests/static-analysis/tail.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+include __DIR__ . '/../../vendor/autoload.php';
+
+use loophp\collection\Collection;
+use loophp\collection\Contract\Collection as CollectionInterface;
+
+/**
+ * @param CollectionInterface<int, int> $collection
+ */
+function tail_checkListInt(CollectionInterface $collection): void
+{
+}
+
+/**
+ * @param CollectionInterface<int, string> $collection
+ */
+function tail_checkListString(CollectionInterface $collection): void
+{
+}
+
+tail_checkListInt(Collection::fromIterable([1, 2, 3])->tail());
+tail_checkListString(Collection::fromIterable(range('a', 'b'))->tail());
