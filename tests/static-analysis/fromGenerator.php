20693c26bf9ebd5a14ffb46c4484c5505190bc6d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-07-23 23:34:23 +0200

    tests: Add static analysis tests.

---
+++ tests/static-analysis/fromGenerator.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+include __DIR__ . '/../../vendor/autoload.php';
+
+use loophp\collection\Collection;
+use loophp\collection\Contract\Collection as CollectionInterface;
+
+/**
+ * @param CollectionInterface<int, int> $collection
+ */
+function fromGenerator_checkList(CollectionInterface $collection): void
+{
+}
+/**
+ * @param CollectionInterface<string, int> $collection
+ */
+function fromGenerator_checkMap(CollectionInterface $collection): void
+{
+}
+/**
+ * @param CollectionInterface<int, int|string> $collection
+ */
+function fromGenerator_checkMixed(CollectionInterface $collection): void
+{
+}
+
+$generatorClosureList = static fn (): Generator => yield from range(1, 3);
+$generatorClosureMap = static fn (): Generator => yield 'myKey' => 1;
+$generatorClosureMixed = static function (): Generator {
+    yield 1 => 2;
+
+    yield 3 => 'b';
+
+    yield 1 => 'c';
+
+    yield 4 => '5';
+};
+
+fromGenerator_checkList(Collection::fromGenerator($generatorClosureList()));
+fromGenerator_checkMap(Collection::fromGenerator($generatorClosureMap()));
+fromGenerator_checkMixed(Collection::fromGenerator($generatorClosureMixed()));
