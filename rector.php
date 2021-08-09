3996db40c10010f785b23ebe2dba4e691f5f174e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-08-09 12:27:04 +0200

    Add rector.php.

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+
+    // Define what rule sets will be applied
+    $containerConfigurator->import(SetList::PHPSPEC_TO_PHPUNIT);
+
+    // get services (needed for register a single rule)
+    // $services = $containerConfigurator->services();
+
+    // register a single rule
+    // $services->set(TypedPropertyRector::class);
+};
