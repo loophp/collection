84c7d2a31a749486ed03968176ad8ba4431eb40a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-08-26 10:46:36 +0200

    tests: Add SA tests.

---
+++ tests/static-analysis/inits.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+include __DIR__ . '/../../vendor/autoload.php';
+
+use loophp\collection\Collection;
+use loophp\collection\Contract\Collection as CollectionInterface;
+
+/**
+ * @param CollectionInterface<int, list<array{0: int, 1: string}>> $collection
+ */
+function inits_checkListString(CollectionInterface $collection): void
+{
+}
+
+/**
+ * @param CollectionInterface<int, list<array{0: string, 1: string}>> $collection
+ */
+function inits_checkMapString(CollectionInterface $collection): void
+{
+}
+
+$listString = range('a', 'c');
+$mapString = array_combine(range('a', 'c'), range('a', 'c'));
+
+inits_checkListString(Collection::fromIterable($listString)->inits());
+inits_checkMapString(Collection::fromIterable($mapString)->inits());
