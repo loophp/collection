84b220d03fafd67aaf2fe63e750ee70141ea2f59
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-08-29 11:14:38 +0200

    docs: Add more examples.

--- docs/pages/code/operations/inits.php
+++ docs/pages/code/operations/inits.php
@@ -13,5 +13,16 @@ use loophp\collection\Collection;
 
 include __DIR__ . '/../../../../vendor/autoload.php';
 
+$input = range('a', 'c');
+
 Collection::fromIterable(range('a', 'c'))
     ->inits(); // [[], [[0, 'a']], [[0, 'a'], [1, 'b']], [[0, 'a'], [1, 'b'], [2, 'c']]]
+
+Collection::fromIterable(array_combine(range('A', 'C'), $input))
+    ->inits(); // [[], [['A', 'a']], [['A', 'a'], ['B', 'b']], [['A', 'a'], ['B', 'b'], ['C', 'c']]]
+
+// To get only the values:
+Collection::fromIterable($input)
+    ->inits()
+    ->map(static fn (array $data): array => array_column($data, 1));
+    // [[], ['a'], ['a', 'b'], ['a', 'b', 'c']]
