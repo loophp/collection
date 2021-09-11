16b62772152d463db6f91aacb135e9822e163706
AlexandruGG <alex.gidei@goodlord.co>
2021-09-11 11:45:12 +0100

    Add `pluck` example

--- docs/pages/code/operations/inits.php
+++ docs/pages/code/operations/inits.php
@@ -22,7 +22,15 @@ Collection::fromIterable(array_combine(range('A', 'C'), $input))
     ->inits(); // [[], [['A', 'a']], [['A', 'a'], ['B', 'b']], [['A', 'a'], ['B', 'b'], ['C', 'c']]]
 
 // To get only the values:
+
+// Using `map` + `array_column`
 Collection::fromIterable($input)
     ->inits()
     ->map(static fn (array $data): array => array_column($data, 1));
+    // [[], ['a'], ['a', 'b'], ['a', 'b', 'c']]
+
+// Using the `pluck` operation
+$var = Collection::fromIterable($input)
+    ->inits()
+    ->pluck('*.1');
     // [[], ['a'], ['a', 'b'], ['a', 'b', 'c']]
