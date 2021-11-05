66566539b38573aa4f2bf3b4f055ca13f967e719
AlexandruGG <alex.gidei@goodlord.co>
2021-11-05 07:16:32 +0000

    Mention PR in php-cs-fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 $config = require __DIR__ . '/vendor/drupol/php-conventions/config/php73/php_cs_fixer.config.php';
 
+// specific file ignore introduced based on discussion: https://github.com/loophp/collection/pull/209#discussion_r739684664
 $config
     ->getFinder()
     ->ignoreDotFiles(false)
