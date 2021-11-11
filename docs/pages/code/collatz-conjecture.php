1da48111339dfc44bc4b4bf5a4bf82145b80d1cb
AlexandruGG <alex.gidei@goodlord.co>
2021-11-11 09:28:00 +0000

    Remove redundant `normalize`

--- docs/pages/code/collatz-conjecture.php
+++ docs/pages/code/collatz-conjecture.php
@@ -18,7 +18,6 @@ $collatz = static fn (int $value): array => 0 === $value % 2
 
 $collection = Collection::unfold($collatz, 10)
     ->unwrap()
-    ->normalize()
-    ->until(static fn ($number): bool => 1 === $number);
+    ->until(static fn (int $number): bool => 1 === $number);
 
 print_r($c->all()); // [25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
