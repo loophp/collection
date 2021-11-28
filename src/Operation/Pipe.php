e3f9131748d0e6ef28f7199a4fe088ee79144d06
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-11-28 21:34:46 +0100

    refactor: Update Pipe operation.

--- src/Operation/Pipe.php
+++ src/Operation/Pipe.php
@@ -53,4 +53,126 @@ final class Pipe extends AbstractOperation
                 static fn (Iterator $iterator): Iterator => $iterator
             );
     }
+
+    /**
+     * @pure
+     *
+     * @template Ik
+     * @template Iv
+     * @template Jk
+     * @template Jv
+     * @template Kk
+     * @template Kv
+     *
+     * @param callable(Iterator<Ik, Iv>): Iterator<Jk, Jv> $op1
+     * @param callable(Iterator<Jk, Jv>): Iterator<Kk, Kv> $op2
+     *
+     * @return Closure(Iterator<Ik, Iv>): Iterator<Kk, Kv>
+     */
+    public static function ofTyped2(callable $op1, callable $op2): Closure
+    {
+        return
+            /**
+             * @param Iterator<Ik, Iv> $iterator
+             *
+             * @return Iterator<Kk, Kv>
+             */
+            static fn (Iterator $iterator): Iterator => $op2($op1($iterator));
+    }
+
+    /**
+     * @pure
+     *
+     * @template Ik
+     * @template Iv
+     * @template Jk
+     * @template Jv
+     * @template Kk
+     * @template Kv
+     * @template Lk
+     * @template Lv
+     *
+     * @param callable(Iterator<Ik, Iv>): Iterator<Jk, Jv> $op1
+     * @param callable(Iterator<Jk, Jv>): Iterator<Kk, Kv> $op2
+     * @param callable(Iterator<Kk, Kv>): Iterator<Lk, Lv> $op3
+     *
+     * @return Closure(Iterator<Ik, Iv>): Iterator<Lk, Lv>
+     */
+    public static function ofTyped3(callable $op1, callable $op2, callable $op3): Closure
+    {
+        return
+            /**
+             * @param Iterator<Ik, Iv> $iterator
+             *
+             * @return Iterator<Lk, Lv>
+             */
+            static fn (Iterator $iterator): Iterator => $op3($op2($op1($iterator)));
+    }
+
+    /**
+     * @pure
+     *
+     * @template Ik
+     * @template Iv
+     * @template Jk
+     * @template Jv
+     * @template Kk
+     * @template Kv
+     * @template Lk
+     * @template Lv
+     * @template Mk
+     * @template Mv
+     *
+     * @param callable(Iterator<Ik, Iv>): Iterator<Jk, Jv> $op1
+     * @param callable(Iterator<Jk, Jv>): Iterator<Kk, Kv> $op2
+     * @param callable(Iterator<Kk, Kv>): Iterator<Lk, Lv> $op3
+     * @param callable(Iterator<Lk, Lv>): Iterator<Mk, Mv> $op4
+     *
+     * @return Closure(Iterator<Ik, Iv>): Iterator<Mk, Mv>
+     */
+    public static function ofTyped4(callable $op1, callable $op2, callable $op3, callable $op4): Closure
+    {
+        return
+            /**
+             * @param Iterator<Ik, Iv> $iterator
+             *
+             * @return Iterator<Mk, Mv>
+             */
+            static fn (Iterator $iterator): Iterator => $op4($op3($op2($op1($iterator))));
+    }
+
+    /**
+     * @pure
+     *
+     * @template Ik
+     * @template Iv
+     * @template Jk
+     * @template Jv
+     * @template Kk
+     * @template Kv
+     * @template Lk
+     * @template Lv
+     * @template Mk
+     * @template Mv
+     * @template Nk
+     * @template Nv
+     *
+     * @param callable(Iterator<Ik, Iv>): Iterator<Jk, Jv> $op1
+     * @param callable(Iterator<Jk, Jv>): Iterator<Kk, Kv> $op2
+     * @param callable(Iterator<Kk, Kv>): Iterator<Lk, Lv> $op3
+     * @param callable(Iterator<Lk, Lv>): Iterator<Mk, Mv> $op4
+     * @param callable(Iterator<Mk, Mv>): Iterator<Nk, Nv> $op5
+     *
+     * @return Closure(Iterator<Ik, Iv>): Iterator<Nk, Nv>
+     */
+    public static function ofTyped5(callable $op1, callable $op2, callable $op3, callable $op4, callable $op5): Closure
+    {
+        return
+            /**
+             * @param Iterator<Ik, Iv> $iterator
+             *
+             * @return Iterator<Nk, Nv>
+             */
+            static fn (Iterator $iterator): Iterator => $op5($op4($op3($op2($op1($iterator)))));
+    }
 }
