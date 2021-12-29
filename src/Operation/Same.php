15bf8dee8ba7dbd147392461dbcaa824cdfab481
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-12-29 21:04:16 +0100

    fix: `Same` operation - proper sign.

--- src/Operation/Same.php
+++ src/Operation/Same.php
@@ -58,7 +58,7 @@ final class Same extends AbstractOperation
                             $other->next();
                         }
 
-                        return yield $iterator->valid() !== $other->valid();
+                        return yield $iterator->valid() === $other->valid();
                     };
     }
 }
