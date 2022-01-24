0fba6dac5dedc04df7d603bc38248aa708d9d67f
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-01-24 19:45:13 +0100

    cs: Cleanup `.php-cs-fixer.dist.php`.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,15 +14,10 @@ $config
     ->getFinder()
     ->ignoreDotFiles(false)
     ->notPath('src/Contract/Operation/Allable.php')
-    // This is for the test "testAllowsDifferentClassesWithSameInterfaceButInDifferentOrder"
-    // See https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/3871
-    ->notPath('tests/unit/Iterator/TypedIteratorTest.php')
     ->name(['.php_cs.dist']);
 
 $rules = $config->getRules();
 
 $rules['return_assignment'] = false;
-// See https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/6179
-$rules['php_unit_dedicate_assert'] = false;
 
 return $config->setRules($rules);
