ab5f9f0817358934a76d5fe9478adb4b8ec55eca
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-06-02 19:52:39 +0200

    fix: Remove `@psalm-suppress` annotation.

--- src/Operation/Collapse.php
+++ src/Operation/Collapse.php
@@ -21,8 +21,6 @@ use Generator;
 final class Collapse extends AbstractOperation
 {
     /**
-     * @psalm-suppress ImpureFunctionCall - using Filter and Flatten as an internal tools, not returned.
-     *
      * @return Closure(iterable<TKey, (T|iterable<TKey, T>)>): Generator<TKey, T>
      */
     public function __invoke(): Closure
