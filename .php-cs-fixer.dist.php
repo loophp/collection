aacb5e9bd965eaf70db656c724bc7045121db7a0
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-09-27 22:43:16 +0200

    refactor: remove files header

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
-
 declare(strict_types=1);
 
 $config = require __DIR__ . '/vendor/drupol/php-conventions/config/php73/php_cs_fixer.config.php';
