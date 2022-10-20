c174e07a32668681cfd032cf77eb0e184e4fa65f
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-10-21 00:29:50 +0200

    fix Collatz example

--- docs/pages/code/collatz-conjecture.php
+++ docs/pages/code/collatz-conjecture.php
@@ -11,8 +11,8 @@ $collatz = static fn (int $value): array => 0 === $value % 2
     ? [$value / 2]
     : [$value * 3 + 1];
 
-$collection = Collection::unfold($collatz, 10)
+$collection = Collection::unfold($collatz, 50)
     ->unwrap()
     ->until(static fn (int $number): bool => 1 === $number);
 
-print_r($c->all()); // [25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+print_r($collection->all()); // [25, 76, 38, 19, 58, 29, 88, 44, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
