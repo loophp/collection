5c7d4015ced04dcc5750506abcfd333256373c1a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-12-28 18:01:42 +0100

    fix: `reduce` operation static annotations

--- src/Operation/Reduce.php
+++ src/Operation/Reduce.php
@@ -19,7 +19,7 @@ final class Reduce extends AbstractOperation
     /**
      * @template V
      *
-     * @return Closure(callable(mixed, mixed, mixed, iterable<mixed, mixed>): mixed): Closure(mixed): Closure(iterable<TKey, T>): Generator<TKey, mixed>
+     * @return Closure(callable(mixed, mixed, mixed, iterable<mixed, mixed>): mixed): Closure(mixed): Closure(iterable<TKey, T>): Generator<int, mixed>
      */
     public function __invoke(): Closure
     {
@@ -27,19 +27,19 @@ final class Reduce extends AbstractOperation
             /**
              * @param callable(V, T, TKey, iterable<TKey, T>): V $callback
              *
-             * @return Closure(V): Closure(iterable<TKey, T>): Generator<TKey, V>
+             * @return Closure(V): Closure(iterable<TKey, T>): Generator<int, V>
              */
             static fn (callable $callback): Closure =>
                 /**
                  * @param V $initial
                  *
-                 * @return Closure(iterable<TKey, T>): Generator<TKey, V>
+                 * @return Closure(iterable<TKey, T>): Generator<int, V>
                  */
                 static fn (mixed $initial): Closure =>
                     /**
                      * @param iterable<TKey, T> $iterable
                      *
-                     * @return Generator<TKey, V>
+                     * @return Generator<int, V>
                      */
                     static fn (iterable $iterable): Generator => yield from new ReduceIterableAggregate($iterable, Closure::fromCallable($callback), $initial);
     }
